2be4b6b7854cf19b7254033d12c8b76de73c7ce4
LukeGiles3 <luke.giles333@gmail.com>
2021-09-07 14:39:08 -0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>this is the welcome page</h1>
+    <h3>go to <a href="/login" class="text-cyan-600 hover:text-cyan-700">login</a> page</h3>
+</body>
+</html>
